2720d61312ffe9773281d639fc142c3f3722dcfe
Hafeez Hamza <hafeez1042@gmail.com>
2019-02-18 11:09:48 +0530

    using state for loading update

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,61 @@
 import * as React from "react";
+import { string } from "prop-types";
 
 export interface ILoadingComponentProps {
-  loading?: boolean;
   label?: string;
   size?: string;
-  positionRelative?: boolean;
 }
 
-const loading = <P extends ILoadingComponentProps>(SpinnerComponent: React.ComponentType<ILoadingComponentProps>) => {
+export interface IComponentState {
+  loading?: boolean;
+  relative?: boolean;
+  label?: string;
+  size?: any;
+}
+
+
+
+const loading = <P extends {}>(
+  SpinnerComponent: React.ComponentType<ILoadingComponentProps>,
+  defaultLabel: string = "",
+  defaultSize?: any,
+  defaultPositionRelative: boolean = false
+) => {
   return (Component: React.ComponentType<P>): React.ComponentType<P> => {
 
-    return class extends React.Component<P, {}> {
+    return class extends React.Component<P, IComponentState> {
+      constructor(props: P) {
+        super(props);
+
+        this.state = {
+          loading: false,
+          relative: defaultPositionRelative,
+          label: defaultLabel,
+          size: defaultSize,
+        }
+
+        this._toggleLoading = this._toggleLoading.bind(this);
+      }
       public render() {
-        if (this.props.loading) {
+        if (this.state.loading) {
           return (
-            <div style={this.props.positionRelative && { position: "relative" }}>
-              <SpinnerComponent />
-              <Component {...this.props} />
+            <div style={this.state.relative ? { position: "relative" } : {}}>
+              <SpinnerComponent label={this.state.label}  size={this.state.size} />
+              <Component {...this.props} toggleLoading={this._toggleLoading} />
             </div>
           )
         }
 
-        return <Component {...this.props} />
+        return <Component {...this.props} toggleLoading={this._toggleLoading} />
+      }
+
+      private _toggleLoading(status?: boolean, relative?: boolean, label?: string, size?: any) {
+        this.setState(prevState => ({
+          loading: status === undefined ? !prevState.loading : status,
+          relative: relative || defaultPositionRelative,
+          label: label || defaultLabel,
+          size: size || defaultSize,
+        }))
       }
     }
   }
